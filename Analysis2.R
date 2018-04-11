72f71828a7df2da08efd57bbb9951121764762dc
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2018-04-11 13:24:27 -0400

    Added the completeness and crossvalidation function

--- Analysis2.R
+++ Analysis2.R
@@ -18,6 +18,50 @@ library(ModelMetrics)
 install.packages('stats')
 library(stats)
 
+# functions----
+
+completeness<-function(dat)
+{
+  dat<-as.data.frame(sapply(dat,as.character))
+  dat[dat=="."]<-NA
+  a=nrow(dat)
+  b=length(dat)
+  c=sum(is.na(dat))/(a*b)
+  return(round(100*(1-c),digits=2))
+}
+
+crossValidate<-function(cvtype,folds,dataset,model,resp)
+{
+  df<-dataset
+  if (cvtype=="kfold")
+  {
+    df$knum<-sample(1:folds,nrow(df),replace = TRUE)
+    rmse_kfold<-0
+    for (i in 1:folds)
+    {
+      df.test<-df[df$knum==i,]
+      df.train<-df[!df$knum==i,]
+      pred<-predict(model,df.test)
+      pred[is.na(pred)]<-mean(pred,na.rm = T)
+      rmse_kfold<-cbind(rmse_kfold,rmse(df.test[,resp],pred))
+    }
+    return (mean(rmse_kfold[,-1]))
+  }
+  else if (cvtype=="LOOCV"||cvtype=="loocv")
+  {
+    rmse_loocv<-0
+    for (i in 1:nrow(df))
+    {
+      df.test<-df[i,]
+      df.train<-df[-i,]
+      pred<-predict(model,df.test)
+      pred[is.na(pred)]<-mean(df.train[,resp])
+      rmse_loocv<-cbind(rmse_loocv,rmse(df.test[,resp],pred))
+    }
+    return(mean(rmse_loocv[,-1]))
+  }
+}
+
 # data----
 
 # AOT Data and processing (1)
